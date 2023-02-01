068d945227d84ac72f15db2e25283ae08e5b3bef
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-01-31 20:20:55 -0600

    initialize react-testing-exercises

---
+++ src/Card.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import Card from './Card';
+
+it('should render', ()=>{
+    render(<Card />)
+})
+
+//snapshot test
+it("matches snapshot", () => {
+    const { asFragment } = render(<Card />); //asFragment is an object 
+    expect(asFragment()).toMatchSnapshot(); //when executed, gives the underlying DOM structure
+    //aka expect the underlyingDOM Structure to match the snapshot (looks in __snapshot__ directory)
+})
